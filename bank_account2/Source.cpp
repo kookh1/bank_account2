fe66236eb0f0059bb5f1f8761d5021cfb05150ca
koo <kookh0987@gmail.com>
2017-05-05 16:51:11 +0900

    [modify] ShowMenu Module

--- bank_account2/Source.cpp
+++ bank_account2/Source.cpp
@@ -31,6 +31,19 @@ void Withdraw();       //출금
 void ShowAllAccount(); //모든 계좌 출력
 
 
+//메뉴 출력
+void ShowMenu()
+{
+	cout << "------Menu-----" << endl;
+	cout << "1. 계좌개설" << endl;
+	cout << "2. 입금" << endl;
+	cout << "3. 출금" << endl;
+	cout << "4. 계좌정보 전체 출력" << endl;
+	cout << "5. 프로그램 종료" << endl;
+	cout << "선택: ";
+}
+
+
 //계좌 만들기
 void MakeAccount()
 {
@@ -70,27 +83,21 @@ int main()
 {
 	while (1)
 	{
-		cout << "------Menu-----" << endl;
-		cout << "1. 계좌개설" << endl;
-		cout << "2. 입금" << endl;
-		cout << "3. 출금" << endl;
-		cout << "4. 계좌정보 전체 출력" << endl;
-		cout << "5. 프로그램 종료" << endl;
+		ShowMenu();   //메뉴출력
 		int sel;
-		cout << "선택: ";
 		cin >> sel;
 
 		switch (sel)
 		{
 		case MAKE:
-			MakeAccount();
+			MakeAccount();  //계좌만들기
 			break;
 		case DEPOSIT:
 			break;
 		case WITHDRAW:
 			break;
 		case SHOW:
-			ShowAllAccount();
+			ShowAllAccount();  //모든계좌출력
 			break;
 		case EXIT:
 			cout << "프로그램을 종료합니다." << endl;
